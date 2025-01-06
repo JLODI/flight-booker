1e1bec014b94674a8caa2ebadc91b78d5c009bdd
JLODI <88990920+JLODI@users.noreply.github.com>
2025-01-05 22:14:42 -0800

    associations added for airport

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,2 +1,4 @@
 class Airport < ApplicationRecord
-end
+    has_many :arriving_flights, class_name: "Flight", foreign_key: "destination_id", inverse_of: :destination_airport, dependent: :destroy
+    has_many :departing_flights, class_name: "Flight", foreign_key: "origin_id", inverse_of: :origin_airport, dependent: :destroy
+end 
