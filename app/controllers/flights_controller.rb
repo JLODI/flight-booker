e895bb49a6fcb4ce73f4512eb4a5da710ab2d9af
JLODI <88990920+JLODI@users.noreply.github.com>
2025-01-06 23:15:19 -0800

    added flights query method and temporary print to view

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,27 @@
 class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.map { |air| air.code }.sort
+    if params[:departure_airport_code].present?
+      @flight_result = search_flights
+    end
+  end
+
+  private
+
+  def search_flights
+    departure_airport_id = Airport.find_by(code: params[:departure_airport_code]).id
+    arrival_airport_id = Airport.find_by(code: params[:arrival_airport_code]).id
+
+    departure_time = Time.parse(params[:departure_time])
+
+    flight_query = Flight
+      .where(departure_airport_id: departure_airport_id)
+      .where(arrival_airport_id: arrival_airport_id)
+      .where("departure_time <= ? AND departure_time >= ?", (departure_time + 5.days), (departure_time - 5.days))
+    if flight_query.empty?
+      "Sorry there are no flights for your query."
+    else
+    flight_query
+    end
   end
 end
