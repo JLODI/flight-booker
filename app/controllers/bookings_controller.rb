27276053e578ca211cea8b302d090812ead602ff
JLODI <88990920+JLODI@users.noreply.github.com>
2025-01-08 21:19:28 -0800

    adjust methods in bookings controller, renamed passengers key to passenger

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,7 @@
 class BookingsController < ApplicationController
     def new
         @booking = Booking.new
+        params[:num_tickets].to_i.times { @booking.build_passenger }
         @num_tickets = params[:num_tickets].to_i
         @flight_option = search_flight_option
     end
@@ -9,7 +10,7 @@ class BookingsController < ApplicationController
         @booking = Booking.new(passenger_params)
         if @booking.save
             flash[:notice] = "Your flight is confirmed!"
-            render :new
+            render index
         else
             flash.now[:error] = "Something went wrong, please try again."
         end
@@ -19,8 +20,7 @@ class BookingsController < ApplicationController
     def search_flight_option
         Flight.find_by(id: params[:flight_id])
     end
-
     def passenger_params
-        params.require(:booking).permit(passengers_attributes: [:name, :email])
+        params.expect(booking: [:flight_id, passenger_attributes: [ :name, :email ] ])
     end
 end
