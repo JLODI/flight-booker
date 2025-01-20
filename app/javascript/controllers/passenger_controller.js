a63a45ce34470cd2a43146eda46a7b40a46ef215
JLODI <88990920+JLODI@users.noreply.github.com>
2025-01-20 01:11:51 -0800

    added passenger stimulus controller

---
+++ app/javascript/controllers/passenger_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="passenger"
+export default class extends Controller {
+  static targets = ['passengerForm', 'addPassengerButton'];
+  static values = { currentPassengerCount: Number, maxPassengerCount: Number }
+
+  connect() {
+  }
+}
