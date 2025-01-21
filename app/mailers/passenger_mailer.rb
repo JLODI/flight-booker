d5f7074810779d07fa44ccf85e110ee84f3d397e
JLODI <88990920+JLODI@users.noreply.github.com>
2025-01-21 00:55:11 -0800

    added base template for mailer and mailer html view

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,11 @@
 class PassengerMailer < ApplicationMailer
+    default from: "booking@odinflightbooker.com"
+
+    def booking_confirmation(passenger)
+        @passenger = params.nil? ? passenger : params[:passenger]
+        @booking = @passenger.booking
+        @flight = @booking.flight
+        mail(to: @passenger.email, subject: "Your Flight has been confirmed")
+    end
+    
 end
